016aa46bd7910ab9126368babd94dbbca58cca34
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-10 05:05:19 -0500

    Removed glow in favor of rgba colors

--- application/views/editor.php
+++ application/views/editor.php
@@ -15,7 +15,9 @@
   <div class="col span_1_of_12">
   </div>
   <div class="col span_7_of_12">
-    <textarea class="poetry-text">Hello world</textarea>
+    <div class="cushion">
+      <textarea class="poetry-text">Hello world</textarea>
+    </div>
   </div>
   <div class="col span_3_of_12">
     <p>Right Panel</p>
