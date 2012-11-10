ff1b21b563a328ba6d9164d83a71f203dea3d111
Gene Demo <demodesigns@gmail.com>
2012-11-10 18:08:46 -0500

    mood

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,6 +2,10 @@
 
 class Home extends MY_Controller {
 	public function index() {
+		$this->template->build("editor");
+	}
+
+	public function sentiment($poem){
 		$url = "http://api.lymbix.com/tonalize_detailed";
 		$headers = array('Content-Type: application/json', 'ACCEPT: application/json', 'VERSION: 2.2', 'AUTHENTICATION: 6c89f5919d00b2aa292c2bb30c71f1900fcec95f');
 		$dataz = array("article: please fucking work", "return_fields: []");
@@ -18,24 +22,6 @@ class Home extends MY_Controller {
  		$ar = json_decode($reply,true);
 
  		echo $ar['dominant_emotion'];
-
-
-		$this->template->build("editor");
-	}
-
-	public function sentiment($poem){
-		$ch = curl_init();
-		$ch = curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'ACCEPT: application/json', 'VERSION: 2.2', 'AUTHENTICATION: 6c89f5919d00b2aa292c2bb30c71f1900fcec95f'));
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		$reply=curl_exec($ch);
-		curl_close($ch);
- 
- 		$ar = json_decode($reply,true);
-
- 		echo $ar['dominant_emotion'];
 	}
 }
 
-?>
