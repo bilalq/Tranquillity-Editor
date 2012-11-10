309192a011f6cf2093cacda96b6e784a1239b4f8
Sharon Li <captainduckie@gmail.com>
2012-11-10 04:54:32 -0500

    css linking

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -15,24 +15,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Grid System & Reset -->
-    <link rel="stylesheet" href="public/css/html5reset.css" media="all">
-    <link rel="stylesheet" href="public/css/col.css" media="all">
-    <link rel="stylesheet" href="public/css/2cols.css" media="all">
-    <link rel="stylesheet" href="public/css/3cols.css" media="all">
-    <link rel="stylesheet" href="public/css/4cols.css" media="all">
-    <link rel="stylesheet" href="public/css/5cols.css" media="all">
-    <link rel="stylesheet" href="public/css/6cols.css" media="all">
-    <link rel="stylesheet" href="public/css/7cols.css" media="all">
-    <link rel="stylesheet" href="public/css/8cols.css" media="all">
-    <link rel="stylesheet" href="public/css/9cols.css" media="all">
-    <link rel="stylesheet" href="public/css/10cols.css" media="all">
-    <link rel="stylesheet" href="public/css/11cols.css" media="all">
-    <link rel="stylesheet" href="public/css/12cols.css" media="all">
+    <link rel="stylesheet" href="public/css/responsive.css" media="all">
 
-    <!-- Responsive Stylesheets -->
-    <link rel="stylesheet" media="only screen and (max-width: 1024px) and (min-width: 769px)" href="public/css/1024.css">
-    <link rel="stylesheet" media="only screen and (max-width: 768px) and (min-width: 481px)" href="public/css/768.css">
-    <link rel="stylesheet" media="only screen and (max-width: 480px)" href="public/css/480.css">
 
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Alex+Brush' rel='stylesheet' type='text/css'>
