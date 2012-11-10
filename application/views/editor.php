f0017435762e6dcd1f6278d1521b63a841f6ae55
Grant Kot <kotsoft@gmail.com>
2012-11-10 04:40:20 -0500

    extended title width

--- application/views/editor.php
+++ application/views/editor.php
@@ -4,12 +4,10 @@
       <img src="public/img/quill.png"/>
     </div>
   </div>
-  <div class="col span_7_of_12">
+  <div class="col span_10_of_12">
     <form>
       <input type="text" name="title" id="title" value="Title">
     </form>
-  </div>
-  <div class="col span_3_of_12">
   </div>
   <div class="col span_1_of_12"></div>
 </div>
