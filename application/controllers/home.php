2fa31ba8a2f9f4c36b1ea5481c7129ff4d2d38b4
Grant Kot <kotsoft@gmail.com>
2012-11-10 01:38:13 -0500

    editor route

---
+++ application/controllers/home.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends MY_Controller {
+  public function index() {
+    $this->template->build("editor");
+  }
+}
