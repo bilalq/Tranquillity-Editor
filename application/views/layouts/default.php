d43bbe4ba495348032bd17d0ec1dace2f4f3a0e4
Grant Kot <kotsoft@gmail.com>
2012-11-10 02:02:38 -0500

    moved code to editor.php

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -3,9 +3,36 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>Poetry IDE</title>
+    <link rel="stylesheet" href="public/css/html5reset.css" media="all">
+    <link rel="stylesheet" href="public/css/col.css" media="all">
+    <link rel="stylesheet" href="public/css/2cols.css" media="all">
+    <link rel="stylesheet" href="public/css/3cols.css" media="all">
+    <link rel="stylesheet" href="public/css/4cols.css" media="all">
+    <link rel="stylesheet" href="public/css/5cols.css" media="all">
+    <link rel="stylesheet" href="public/css/6cols.css" media="all">
+    <link rel="stylesheet" href="public/css/7cols.css" media="all">
+    <link rel="stylesheet" href="public/css/8cols.css" media="all">
+    <link rel="stylesheet" href="public/css/9cols.css" media="all">
+    <link rel="stylesheet" href="public/css/10cols.css" media="all">
+    <link rel="stylesheet" href="public/css/11cols.css" media="all">
+    <link rel="stylesheet" href="public/css/12cols.css" media="all">
+
+    <!-- Responsive Stylesheets -->
+    <link rel="stylesheet" media="only screen and (max-width: 1024px) and (min-width: 769px)" href="public/css/1024.css">
+    <link rel="stylesheet" media="only screen and (max-width: 768px) and (min-width: 481px)" href="public/css/768.css">
+    <link rel="stylesheet" media="only screen and (max-width: 480px)" href="public/css/480.css">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="public/css/main.css" media="all">
+
+    <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements and feature detects -->
+    <script src="/js/modernizr-2.5.3-min.js"></script>
+
   </head>
   <body>
   <h1>Poetry IDE</h1>
     <?= $template['body'] ?>
+
+    <script src="js/responsivegridsystem.js"></script>
   </body>
 </html>
