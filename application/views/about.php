dc877994dcc2e61fea99ec14ca0588f92a4c54de
Gene Demo <demodesigns@gmail.com>
2012-11-10 17:43:48 -0500

    added about page

--- application/views/about.php
+++ application/views/about.php
@@ -1,10 +1,32 @@
 <div class="about row">
   <div class="twelve columns">
-    <h1>About</h1>
 
     <div class="row">
-      <div class="twelve columns">
-        This is a text editor built with poets in mind. Everything is finely tuned to assist with the creative process. Blah blah blah...
+      <div class="one columns">
+      </div>
+      <div class="eleven columns">
+        <h1>About</h1>
+
+        <p>This is a text editor built with poets in mind. Everything is finely tuned to assist with the creative process. </p>
+
+		<p>Your soul, an inkwell -<br />
+		A web app to help you create<br />
+		Of Hack N Jill borne</p>
+
+		<h1>Features</h1>
+
+		<p>Its features include the below:<br />
+		Rhyme helper, Syllable count.<br />
+		Let your inner Creative grow:<br />
+		Your thoughts, the fount.</p>
+
+		<h1>Technology at Work</h1>
+
+		<p>Foundation for a responsive interface,<br />
+		Code Igniter to keep order in haste,<br />
+		RhymeBrain to turn out rhyming words;<br />
+		Proud tools, these, of tech nerds.</p>
+
       </div>
     </div>
   </div>
