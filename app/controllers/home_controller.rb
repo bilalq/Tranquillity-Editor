669f381994d4446b2c7c8af659fb5d9f23443e9b
Grant Kot <kotsoft@gmail.com>
2013-01-19 00:32:13 -0500

    Bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    if session[:poem]
-      if user_signed_in?
-        current_user.poems.create session[:poem]
-        session[:poem] = nil
-        redirect_to my_poems_poems_path
-      end
+    if session[:poem] && user_signed_in?
+      current_user.poems.create session[:poem]
+      session[:poem] = nil
+      redirect_to my_poems_poems_path
     else
       @poem = Poem.new
     end
